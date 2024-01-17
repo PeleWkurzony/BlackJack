d65417de263ba4be8fcd194918f69b93051d5a3f
Pele <banskimateusz7@gmail.com>
2024-01-17 20:44:33 +0100

    Make a clean in client app

--- WebApp/ClientApp/src/components/Layout.js
+++ WebApp/ClientApp/src/components/Layout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
+import { NavMenuOld } from './NavMenu-old';
 
 export class Layout extends Component {
   static displayName = Layout.name;
---
+++ WebApp/ClientApp/src/components/NavMenu-old.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import './NavMenu.css';
+
+//TODO: Tutaj b�dzie aktualny stan konta i link do otworzenia ustawie�
+
+export class NavMenuOld extends Component {
+  static displayName = NavMenuOld.name;
+
+  constructor (props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar () {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
+  render() {
+    return (
+      <header>
+        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
+          <NavbarBrand tag={Link} to="/">BlackJack</NavbarBrand>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+            <ul className="navbar-nav flex-grow">
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+              </NavItem>
+            </ul>
+          </Collapse>
+        </Navbar>
+      </header>
+    );
+  }
+}
