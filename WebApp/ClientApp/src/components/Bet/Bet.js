5d1bb734e3cb1016827d29681af5ecd557c4f3d2
Przemek765 <prpjade12345@gmail.com>
2024-02-29 08:37:07 +0100

    Add a comment to component Bet.js

--- WebApp/ClientApp/src/components/Bet/Bet.js
+++ WebApp/ClientApp/src/components/Bet/Bet.js
@@ -1,25 +1,42 @@
-import {useContext, useState} from 'react';
+import { useContext, useState } from 'react';
 import { MoneyBet } from "./MoneyBet";
 import { BetController } from './BetController';
 import './Bet.scss';
 import { ProfileContext } from "../Context/ProfileContext";
 
+/**
+ * Represents a component managing the betting process.
+ * @returns {JSX.Element} The JSX element representing the Bet component.
+ */
 export const Bet = () => {
-    
+
+    /**
+     * State indicating whether a bet has been placed.
+     * @type {[boolean, function]} Tuple with state value and setter function.
+     */
     const [isBet, setIsBet] = useState(false);
+
+    /**
+     * User profile data obtained from context.
+     * @type {object}
+     */
     const profile = useContext(ProfileContext);
-    
+
+    /**
+     * Conditional rendering of components based on 'isBet' state.
+     * @returns {JSX.Element} The JSX element representing the BetController or MoneyBet component.
+     */
     if (isBet === true) {
         return (
-            <BetController onAction={(action) => {console.log(action, profile.moneyBet);}} />
+            <BetController onAction={(action) => { console.log(action, profile.moneyBet); }} />
         )
     }
     else if (isBet === false) {
         return (
             <MoneyBet onBetAccept={() => {
                 setIsBet(true);
-            }}/>
-        )   
+            }} />
+        )
     }
-    
+
 }
