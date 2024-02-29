f7003aeb93ef5f0a6b1eec5d98b0dbede48bbaa8
Przemek765 <prpjade12345@gmail.com>
2024-02-29 08:59:10 +0100

    Add a comment to component SettingComponent.js

--- WebApp/ClientApp/src/components/NavMenu/SettingsComponent.js
+++ WebApp/ClientApp/src/components/NavMenu/SettingsComponent.js
@@ -1,10 +1,17 @@
-﻿
-
+﻿/**
+ * Represents a component for displaying settings.
+ * @returns {JSX.Element} The JSX element representing the SettingsComponent.
+ */
 export const SettingsComponent = () => {
-    
+
+    /**
+     * Renders the component displaying settings.
+     * @returns {JSX.Element} The JSX element representing the SettingsComponent.
+     */
     return (
         <div id="nav-menu-settings-container">
+            {/* Render settings icon */}
             <i id="nav-menu-settings-icon" className="bi bi-gear"></i>
         </div>
-    )
+    );
 }
