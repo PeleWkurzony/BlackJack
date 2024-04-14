821a54291a785a5dc7b9eb2aa02bee053bd0f1d4
Pele <banskimateusz7@gmail.com>
2024-04-14 18:27:26 +0200

    Refactor

--- WebApp/ClientApp/src/components/Home/Croupier.jsx
+++ WebApp/ClientApp/src/components/Home/Croupier.jsx
@@ -1,6 +1,7 @@
 ﻿export const Croupier = () => {
     
     const token = '/Others/coinFall.png';
+    const token2 = '/Others/coinFall2.png'
     
     return (
         <div id="croupier">
@@ -12,6 +13,11 @@
             <img id="token-fall"  alt="" className="left" src={token}></img>
             <img id="token-fall"  alt="" className="left2" src={token}></img>
             <img id="token-fall"  alt="" className="right" src={token}></img>
+            
+            <img id="token-fall" alt="" className="right2" src={token2}></img>
+            <img id="token-fall"  alt="" className="left" src={token2}></img>
+            <img id="token-fall"  alt="" className="left2" src={token2}></img>
+            <img id="token-fall"  alt="" className="right" src={token2}></img>
         </div>
     )
 }
