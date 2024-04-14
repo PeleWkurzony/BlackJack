c0d2ff8a8d1c2be005fd5126cd22eef0262ab337
Przemek765 <prpjade12345@gmail.com>
2024-04-14 19:05:58 +0200

    Add a comment to component ProfileSlice.js

--- WebApp/ClientApp/src/components/Context/ProfileSlice.js
+++ WebApp/ClientApp/src/components/Context/ProfileSlice.js
@@ -1,31 +1,60 @@
 ﻿import { createSlice, configureStore } from "@reduxjs/toolkit";
 
+// Initial state for the profile slice
 const initialState = {
+    // The amount of money the player has
     money: 3000,
+    // The current bet amount
     bet: 123,
+    // The amount of money reserved for insurance
     insuranceMoney: 0
 };
 
+/**
+ * Redux slice for managing the player's profile.
+ * @type {Slice}
+ */
 export const profileSlice = createSlice({
-    name: 'profile',
-    initialState,
+    name: 'profile',    // Name of the slice
+    initialState,       // Initial state
     reducers: {
+        /**
+         * Reducer for changing the player's money.
+         * @param {object} state - The current state of the profile slice.
+         * @param {number} action.payload - The new value for money.
+         */
         changeMoney: (state, action) => {
             state.money = action.payload;
         },
+        /**
+         * Reducer for changing the player's current bet.
+         * @param {object} state - The current state of the profile slice.
+         * @param {number} action.payload - The new value for bet.
+         */
         changeBet: (state, action) => {
             state.bet = action.payload;
         },
+        /**
+         * Reducer for setting the amount of money reserved for insurance.
+         * @param {object} state - The current state of the profile slice.
+         * @param {number} action.payload - The new value for insuranceMoney.
+         */
         setInsuranceMoney: (state, action) => {
             state.insuranceMoney = action.payload;
         }
     }
 });
 
-export const { 
+// Export individual action creators
+export const {
     changeMoney,
     changeBet,
     setInsuranceMoney
 } = profileSlice.actions;
 
-
+// Configure the Redux store with the profile slice
+export const store = configureStore({
+    reducer: {
+        profile: profileSlice.reducer
+    }
+});
