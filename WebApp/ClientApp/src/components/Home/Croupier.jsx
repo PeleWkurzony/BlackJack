1d42af588c34e059ed89718463866c8ecda9321b
Pele <banskimateusz7@gmail.com>
2024-04-14 18:20:15 +0200

    Small refactor after merge

---
+++ WebApp/ClientApp/src/components/Home/Croupier.jsx
@@ -0,0 +1,17 @@
+﻿export const Croupier = () => {
+    
+    const token = '/Others/coinFall.png';
+    
+    return (
+        <div id="croupier">
+
+            <img id="croupierImg" src="/Others/krupier1.png" alt="croupier" />
+            <img id="token-fall" alt="" className="right" src={token}></img>
+
+            <img id="token-fall" alt="" className="right2" src={token}></img>
+            <img id="token-fall"  alt="" className="left" src={token}></img>
+            <img id="token-fall"  alt="" className="left2" src={token}></img>
+            <img id="token-fall"  alt="" className="right" src={token}></img>
+        </div>
+    )
+}
