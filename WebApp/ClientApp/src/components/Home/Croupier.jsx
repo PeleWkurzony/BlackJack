2244d383b0b3b7c53752c748900a89ef5a9c18b9
Przemek765 <prpjade12345@gmail.com>
2024-04-14 19:08:27 +0200

    Add a comment to component Croupier.jsx

--- WebApp/ClientApp/src/components/Home/Croupier.jsx
+++ WebApp/ClientApp/src/components/Home/Croupier.jsx
@@ -1,23 +1,30 @@
-﻿export const Croupier = () => {
-    
+﻿/**
+ * Component representing the croupier in the game.
+ * @returns {JSX.Element} The JSX element representing the Croupier component.
+ */
+export const Croupier = () => {
+
+    // Token image URLs
     const token = '/Others/coinFall.png';
     const token2 = '/Others/coinFall2.png'
-    
+
+    // Render the croupier component
     return (
         <div id="croupier">
-
+            {/* Croupier image */}
             <img id="croupierImg" src="/Others/krupier1.png" alt="croupier" />
+
+            {/* Falling tokens */}
+            <img id="token-fall" alt="" className="right" src={token}></img>
+            <img id="token-fall" alt="" className="right2" src={token}></img>
+            <img id="token-fall" alt="" className="left" src={token}></img>
+            <img id="token-fall" alt="" className="left2" src={token}></img>
             <img id="token-fall" alt="" className="right" src={token}></img>
 
-            <img id="token-fall" alt="" className="right2" src={token}></img>
-            <img id="token-fall"  alt="" className="left" src={token}></img>
-            <img id="token-fall"  alt="" className="left2" src={token}></img>
-            <img id="token-fall"  alt="" className="right" src={token}></img>
-            
             <img id="token-fall" alt="" className="right2" src={token2}></img>
-            <img id="token-fall"  alt="" className="left" src={token2}></img>
-            <img id="token-fall"  alt="" className="left2" src={token2}></img>
-            <img id="token-fall"  alt="" className="right" src={token2}></img>
+            <img id="token-fall" alt="" className="left" src={token2}></img>
+            <img id="token-fall" alt="" className="left2" src={token2}></img>
+            <img id="token-fall" alt="" className="right" src={token2}></img>
         </div>
-    )
+    );
 }
