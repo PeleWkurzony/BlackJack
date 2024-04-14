b3f2fc5a8a6fcbd16a9c39791df0dcc65cac86d1
Przemek765 <prpjade12345@gmail.com>
2024-04-14 19:04:22 +0200

    Add a comment to component ActionsContext.js

--- WebApp/ClientApp/src/components/Context/ActionsContext.js
+++ WebApp/ClientApp/src/components/Context/ActionsContext.js
@@ -1,37 +1,64 @@
 ﻿import { createSlice } from "@reduxjs/toolkit";
 
+// Initial state for the actions slice
 const initialState = {
-    canDouble: true,
-    canSplit: false,
-    canInsurance: false
+    canDouble: true,    // Indicates whether the player can double down
+    canSplit: false,    // Indicates whether the player can split their hand
+    canInsurance: false // Indicates whether the player can take insurance
 };
 
+/**
+ * Redux slice for managing available actions in the game.
+ * @type {Slice}
+ */
 export const actionsSlice = createSlice({
-    name: 'actions',
-    initialState,
+    name: 'actions',    // Name of the slice
+    initialState,       // Initial state
     reducers: {
+        /**
+         * Reducer for setting the availability of doubling down.
+         * @param {object} state - The current state of the actions slice.
+         * @param {boolean} action.payload - The new value for canDouble.
+         */
         setCanDouble: (state, action) => {
             state.canDouble = action.payload;
         },
+        /**
+         * Reducer for setting the availability of splitting.
+         * @param {object} state - The current state of the actions slice.
+         * @param {boolean} action.payload - The new value for canSplit.
+         */
         setCanSplit: (state, action) => {
             state.canSplit = action.payload;
         },
+        /**
+         * Reducer for setting the availability of insurance.
+         * @param {object} state - The current state of the actions slice.
+         * @param {boolean} action.payload - The new value for canInsurance.
+         */
         setCanInsurance: (state, action) => {
             state.canInsurance = action.payload;
         },
+        /**
+         * Reducer for resetting all action availabilities to default values.
+         * @param {object} state - The current state of the actions slice.
+         */
         resetActions: (state) => {
             state.canDouble = true;
             state.canSplit = false;
             state.canInsurance = false;
         }
-    }});
+    }
+});
 
-export const { 
+// Export individual action creators
+export const {
     setCanDouble,
     setCanSplit,
     setCanInsurance,
     resetActions
 } = actionsSlice.actions;
+
 
 
 
