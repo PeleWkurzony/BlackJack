d33e0c3fd6e41de9a07332f8f38282207fb61139
Przemek765 <prpjade12345@gmail.com>
2024-04-14 19:17:07 +0200

    Add a comment to component Home.js

--- WebApp/ClientApp/src/components/Home.js
+++ WebApp/ClientApp/src/components/Home.js
@@ -2,11 +2,20 @@ import { Bet } from './Bet/Bet';
 import "./Style.scss";
 import { Croupier } from './Home/Croupier';
 
+/**
+ * Represents the main screen component of the game.
+ * @returns {JSX.Element} The JSX element representing the Home component.
+ */
 export const Home = () => {
+
+    /**
+     * Renders the main screen of the game.
+     * @returns {JSX.Element} The JSX element representing the Home component.
+     */
     return (
         <>
             <Croupier />
             <Bet />
         </>
-    )
-};
+    );
+}
