dd770bd6838673635e718a6d8e7f311c8fbb4758
Przemek765 <prpjade12345@gmail.com>
2024-04-14 19:06:44 +0200

    Add a comment to component Store.js

--- WebApp/ClientApp/src/components/Context/Store.js
+++ WebApp/ClientApp/src/components/Context/Store.js
@@ -4,11 +4,16 @@ import { cardSlice } from "./CardSlice";
 import { actionsSlice } from "./ActionsContext";
 import { gameManagerSlice } from "./GameManageSlice";
 
+/**
+ * Redux store configuration.
+ * @type {Store}
+ */
 export const store = configureStore({
     reducer: {
-        profile: profileSlice.reducer,
-        card: cardSlice.reducer,
-        actions: actionsSlice.reducer,
-        gameManager: gameManagerSlice.reducer
+        // Combine reducers for different slices of state
+        profile: profileSlice.reducer,         // Reducer for managing player's profile
+        card: cardSlice.reducer,               // Reducer for managing cards in the game
+        actions: actionsSlice.reducer,         // Reducer for managing available actions in the game
+        gameManager: gameManagerSlice.reducer  // Reducer for managing game state
     }
 });
