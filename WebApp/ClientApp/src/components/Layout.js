4f49d582a7a9a343ac90947959cfa79e39944964
Przemek765 <prpjade12345@gmail.com>
2024-04-14 19:18:06 +0200

    Add a comment to component Layout.js

--- WebApp/ClientApp/src/components/Layout.js
+++ WebApp/ClientApp/src/components/Layout.js
@@ -4,19 +4,30 @@ import { NavMenu } from "./NavMenu/NavMenu";
 import { store } from "./Context/Store";
 import { Provider } from "react-redux";
 
+/**
+ * Represents the layout component of the application.
+ */
 export class Layout extends Component {
-  static displayName = Layout.name;
+    /**
+     * The display name of the Layout component.
+     */
+    static displayName = Layout.name;
 
-  render() {
-    return (
-      <div>
-          <Provider store={store}>
-              <NavMenu />
-              <Container tag="main">
-                  {this.props.children}
-              </Container>
-          </Provider>
-  </div>
-    );
-  }
+    /**
+     * Renders the layout component.
+     * @returns {JSX.Element} The JSX element representing the Layout component.
+     */
+    render() {
+        return (
+            <div>
+                <Provider store={store}>
+                    <NavMenu />
+                    <Container tag="main">
+                        {this.props.children}
+                    </Container>
+                </Provider>
+            </div>
+        );
+    }
 }
+
