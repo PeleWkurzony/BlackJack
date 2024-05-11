0acfa330537ac8171673036456d66ae1e5f0e989
marpancel <pancmarcel@gmail.com>
2024-05-11 15:25:53 +0200

    html update

--- WebApp/ClientApp/src/components/GameOver/GameOver.jsx
+++ WebApp/ClientApp/src/components/GameOver/GameOver.jsx
@@ -6,16 +6,12 @@ export const GameOver = () => {
     return (
         <>
             <div class="game-container">
-                <header>
-                    <h1>Blackjack Game</h1>
-                </header>
                 <section class="game-section">
-                    <h2>Game Over</h2>
-                    <p>Your score: 18</p>
+                    <h1>Game Over</h1>
                     <button id="restart-btn">Start Again</button>
                 </section>
                 <footer>
-                    <p>Blackjack Game</p>
+                    <p>Blackjack Casino</p>
                 </footer>
             </div>
         </>
