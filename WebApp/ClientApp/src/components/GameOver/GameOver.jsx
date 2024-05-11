f788f74a0b971114e89b8e3ecfa726ddd9c4f186
marpancel <pancmarcel@gmail.com>
2024-05-11 15:39:38 +0200

    styling update

--- WebApp/ClientApp/src/components/GameOver/GameOver.jsx
+++ WebApp/ClientApp/src/components/GameOver/GameOver.jsx
@@ -8,7 +8,6 @@ export const GameOver = () => {
             <div class="game-container">
                 <section class="game-section">
                     <h1>Game Over</h1>
-                    <p>Your score: 18</p>
                     <button id="restart-btn">Start Again</button>
                 </section>
 
